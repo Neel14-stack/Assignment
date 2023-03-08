56e2a94e947c39b9ad786119f2853c5d608d179d
Neel14-stack <nilmr45@gmail.com>
2023-03-08 13:11:28 +0530

    CTE

---
+++ Practise/SQL/CTE.sql
@@ -0,0 +1,10 @@
+-- CTE
+-- CTE: COMMON TABLE EXPRESSION which is use to construct complex queries into more readble format
+
+-- CONDITION:
+	-- MYSQL version > 8.0
+-- CTE is just like derieved table, CTE is not stored as an object and last only during execution of a query statement.
+
+-- SYNTAX
+	-- with CTE_NAME (COLUMN LIST) AS 
+	-- 	  ( WRITE QUERY ) SELECT * FROM CTE_NAME;
