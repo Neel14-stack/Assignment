190e4bfb916970700c9483572e96dca9d0c67099
Neel14-stack <nilmr45@gmail.com>
2023-03-08 14:40:23 +0530

    cte updated

--- Practise/SQL/CTE.sql
+++ Practise/SQL/CTE.sql
@@ -8,3 +8,28 @@
 -- SYNTAX
 	-- with CTE_NAME (COLUMN LIST) AS 
 	-- 	  ( WRITE QUERY ) SELECT * FROM CTE_NAME;
+    
+-- Fetch employee who earn more than average salary of all employees
+with average_salary (avg_sal) as (select avg(salary) from employee) select * from employee emp, average_salary av where
+emp.salary > av.avg_sal;
+
+with salesdept as (
+select empid, empname, manager_id
+from tek_employee
+where department_id = 'd1')
+select empname, manager_id from salesdept
+where manager_id = 'm1';
+
+-- Derived Table
+-- Derived table is a virtual table returned from select statement. A derived table is similar to temporary table but
+-- this is much simpler
+
+-- syntax
+-- select column_llist from
+-- (select column_list from table_1) derived_table_name
+-- where derived_table_name.column > value;
+
+select salary
+from (select avg(salary) as avg_sal from tek_employee) avg_derived_table
+inner join tek_employee
+where salary > avg_derived_table.avg_sal;
