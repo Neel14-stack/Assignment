231239f23fa22479179cb0bcdae1de93fbb196bb
Neel14-stack <nilmr45@gmail.com>
2023-03-10 10:35:14 +0530

    row number

--- Practise/SQL/window.sql
+++ Practise/SQL/window.sql
@@ -19,4 +19,8 @@ select *, max(income) over() from hr_employee;
 select hremp.employeeid, hremp.age, hremp.department, hremp.jobrole, 
 max(income) over(partition by jobrole) as max_jobrole_income from hr_employee as hremp;
 
-
+-- ROWNUMBER()
+-- write a query which will show first 3 employee from each jobrole to join the country
+select employeeid from hr_employee order by employeeid limit 3;
+select hremp.employeeid, hremp.age, hremp.department, hremp.jobrole, max(income) over (partition by jobrole) 
+as max_jobrole_income from hr_employee as hremp;
