ff46f970abe46a3853b0c5be642cc3ed10fd0f14
Neel14-stack <nilmr45@gmail.com>
2023-03-13 11:34:25 +0530

    one more testing

--- Python/Pytest/unit testing/shopping_cart.py
+++ Python/Pytest/unit testing/shopping_cart.py
@@ -11,19 +11,29 @@ Created on Fri Mar 10 17:48:40 2023
 from typing import List
 
 class MyshoppingCart:
-    def __init__(self) -> None:
+    def __init__(self,max_size : int) -> None:
         self.items: List[str] = []
+        self.max_size = max_size
         pass
     
     def add_to_cart(self,item:str):
         self.items.append(item)
-        pass
+        #try change max_size - 1, max_size - 2
+        # unit test will still pass
+    
+# =============================================================================
+#         if self.size_of_cart() == self.max_size:
+#             raise OverflowError("cannot add more items")
+#         self.items.append(item)
+#     
+# =============================================================================
     
     def size_of_cart(self) -> int:
         return len(self.items)
         pass
     
     def get_items(self) -> List[str]:
+        return self.items
         pass
     
     def get_total_price(self, price_map):
--- Python/Pytest/unit testing/test_shopping_cart.py
+++ Python/Pytest/unit testing/test_shopping_cart.py
@@ -6,8 +6,31 @@ Created on Fri Mar 10 17:53:54 2023
 """
 
 from shopping_cart import MyshoppingCart
+# =============================================================================
+# 
+# def test_add_to_cart():
+#     cart = MyshoppingCart(5)
+#     cart.add_to_cart('Nike Airforce1')
+#     # debugging the code in python using assert
+#     assert cart.size_of_cart() == 1
+# =============================================================================
+    
 
-def test_add_to_cart():
-    cart = MyshoppingCart()
-    cart.add_to_cart('Nike Airforce')
-    assert cart.size_of_cart() == 1
+def test_when_item_added_then_cart_contains_item():
+    cart = MyshoppingCart(5)
+    cart.add_to_cart("neel")
+    # test if product is in cart.get_itms()
+    #print('Nike Airforce1' in cart.get_items())
+    assert "neel" in cart.get_items()
+    
+# =============================================================================
+# 
+# def test_when_add_more_than_max_items_should_fail():
+#     # 5 is maximum item
+#     cart = MyshoppingCart(5)
+#     with pytest.raises(OverflowError):
+#         for _ in range(7):
+#             cart.add_to_cart('Nike Airforce1')
+# 
+# =============================================================================
+        
