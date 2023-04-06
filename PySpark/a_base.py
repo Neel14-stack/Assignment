275323a4d5776422c442572f7d73233e347c0257
Neel14-stack <nilmr45@gmail.com>
2023-04-06 15:13:59 +0530

    sql alchemy in visual studio code

---
+++ PySpark/a_base.py
@@ -0,0 +1,16 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class Restaurant(Base):
+    __tablename__ = 'restaurant'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+    
