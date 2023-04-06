11ea2dd2100c8e7278257fd974ad1bc715d06cc8
Neel14-stack <nilmr45@gmail.com>
2023-04-06 15:24:24 +0530

    sql alchemy in visual studio code

--- PySpark/a_base.py
+++ PySpark/a_base.py
@@ -7,10 +7,25 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+# class as a table name in case of ORM
 class Restaurant(Base):
     __tablename__ = 'restaurant'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
-    
+class MenuItem(Base):
+    __tablename__ = 'menu_item'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    price = Column(String(8))
+    course = Column(String(250))
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+    restaurant = relationship(Restaurant)
+
+
+if __name__ == '__main__':
+    engine = create_engine('sqlite:///restaurantmenu.db')
+    Base 
