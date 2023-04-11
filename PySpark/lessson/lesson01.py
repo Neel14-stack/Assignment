0bc77569f725f2c869303ec28e4298ba361f030f
Neel14-stack <nilmr45@gmail.com>
2023-04-11 10:44:55 +0530

    lesson1 py file modified

--- PySpark/lessson/lesson01.py
+++ PySpark/lessson/lesson01.py
@@ -7,4 +7,16 @@ class HelloWorld:
     def __init__(self):
         os.environ['PYSPARK_PYTHON'] = sys.executable
         os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
-        
+
+    def initSpark(self):
+        spark = SparkSession.builder.master("local[*]").getOrCreate()
+        sc = spark.sparkContext
+        return spark,sc
+    
+
+if __name__=='__main__':
+    hworld = HelloWorld()
+    spark,sc = hworld.initSpark()
+    data = ['Welcome to PySpark','Hello World!!']
+    rdd1 = sc.parallelize(data)
+    print(rdd1.collect())
